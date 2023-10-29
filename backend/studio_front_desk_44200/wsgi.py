5b1f43b658c5665b19aab6cc62b120a8a21593cd
Crowdbotics <team@crowdbotics.com>
2023-10-29 05:11:50 +0000

    Initial commit

---
+++ backend/studio_front_desk_44200/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for studio_front_desk_44200 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "studio_front_desk_44200.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "studio-front-desk-44200",
+  url: "https://studio-front-desk-44200.botics.co",
+  api: "https://studio-front-desk-44200.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
